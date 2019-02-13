cc5d0f964a1743badaabe3683980c92181f62b42
ssxrgio <scarreras10@gmail.com>
2019-02-13 16:13:16 +0100

    Main program for seq class development.

---
+++ P1/Seq_main.py
@@ -0,0 +1,12 @@
+from Seq import seq  # Importing the file.'''
+
+s1 = seq('AATCGATAGTGC') # Converting a string into type seq.
+s2 = seq('AAAATCGCTGATAGCTAGATCT')
+s3 = seq_1.reverse()
+s4 = seq_2.complement()
+
+len_1 = seq_1.len()
+len_2 = seq_2.len()
+len_rev = seq1_rev.len()
+len_comp = seq2_comp.len()
+
