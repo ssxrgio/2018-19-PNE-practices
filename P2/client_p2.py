b13f75b36d9f0aea46cb57973086e26089fd5d17
ssxrgio <scarreras10@gmail.com>
2019-02-13 21:42:27 +0100

    Loop adjustments.

--- P2/client_p2.py
+++ P2/client_p2.py
@@ -2,9 +2,11 @@ import socket
 from Seq import seq
 
 port = 8081
-IP = '212.128.253.96'
+IP = '192.168.0.160'
 
-while True:
+cond = True
+
+while cond:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('Socket created successfully.\n')
     s.connect((IP, port))
@@ -12,12 +14,11 @@ while True:
     sequence = input('Type a valid DNA sequence: ').upper()
 
     if sequence == 'EXIT':
-        False
+        cond = False
 
     else:
         sequence = seq(sequence)
         seq_rev = seq(sequence.reverse())
         seq_comp = seq_rev.complement()
         s.send(str.encode(('Reverse-complementary sequence: {}'.format(seq_comp))))
-
         s.close()
