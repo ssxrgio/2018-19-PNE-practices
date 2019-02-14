6bc82ed788ab6d0b994cd447d0a5d411e528b2e9
ssxrgio <scarreras10@gmail.com>
2019-02-14 17:35:54 +0100

    P2 Extra.

---
+++ P2-Extra/server_2ex.py
@@ -0,0 +1,48 @@
+import socket
+from Seq import seq
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "192.168.56.1"
+MAX_OPEN_REQUESTS = 5
+
+# Counting the number of connections
+number_con = 0
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    serversocket.bind((IP, PORT))
+    # become a server socket
+    # MAX_OPEN_REQUESTS connect requests before refusing outside connections
+    serversocket.listen(MAX_OPEN_REQUESTS)
+
+    while True:
+        # accept connections from outside
+        print("Waiting for connections at {}, {} ".format(IP, PORT))
+        (clientsocket, address) = serversocket.accept()
+
+        # Another connection!e
+        number_con += 1
+
+        # Print the conection number
+        print("CONNECTION: {}. From the IP: {}".format(number_con, address))
+
+        # Read the message from the client, if any
+        msg = clientsocket.recv(2048).decode("utf-8")
+        print("Sequence from client: {}".format(msg))
+
+        # Send the message
+        msg = seq(msg)
+        msg_comp = msg.complement()
+        send_bytes = str.encode(msg_comp)
+        # We must write bytes, not a string
+        clientsocket.send(send_bytes)
+        clientsocket.close()
+
+except socket.error:
+    print("Problems using port {}. Do you have permission?".format(PORT))
+
+except KeyboardInterrupt:
+    print("Server stopped by the user")
+    serversocket.close()
