ff7296f074fc7a80a5de79818a31424661eafea6
ssxrgio <Pergamino27>
2019-02-19 16:00:48 +0100

    Client.

--- session-9/client.py
+++ session-9/client.py
@@ -5,13 +5,12 @@ IP = "212.128.253.106"
 PORT = 8081
 
 while True: # Instead of connecting the first time, just ask the user for the message so the server can attend other clients until the message is sent.
+    msg = input("> ")
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # establish the connection to the Server (IP, PORT)
     s.connect((IP, PORT))
-
-    # The client is blocking the server....  NOT A GOOD DESIGN!!!
-    msg = input("> ")
 
     # Send the request message to the server
     s.send(str.encode(msg))
