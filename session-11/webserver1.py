b54138d7fb5bdf6670fdec21d3b31145045fd653
ssxrgio <Pergamino27>
2019-02-26 16:27:43 +0100

    Werb server 1.

---
+++ session-11/webserver1.py
@@ -0,0 +1,71 @@
+import socket
+# import termcolor
+
+# Change this IP to yours!!!!!
+IP = "212.128.253.100"
+PORT = 8088
+MAX_OPEN_REQUESTS = 5
+
+def process_client(cs):
+    """Process the client request.
+    Parameters:  cs: socket for communicating with the client"""
+
+    # Read client message. Decode it as a string
+    msg = cs.recv(2048).decode("utf-8")
+
+    # Print the received message, for debugging
+    print()
+    print("Request message: ")
+    #termcolor.cprint(msg, 'green')
+    print(msg)
+
+    # Print the received message.
+    print()
+    print('Request Message: ')
+    # termcolor.cprint(msg, 'blue')
+    print(msg)
+
+    content = 'Hello! I am always depressed! :)'
+
+    # store the first line into one variable
+    # always use 1.1
+
+    status_line = 'HTTP/1.1 200 ok\r\n'
+
+    header = 'Content-Type: text/plain\r\n'
+    header += 'Content-Lenght: {}\r\n'.format((len(str.encode(content))))
+
+    response_msg = status_line + header + '\r\n' + content
+
+    cs.send(str.encode(response_msg))
+
+
+    # Close the socket
+    cs.close()
+
+
+# MAIN PROGRAM
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the IP and PORT
+serversocket.bind((IP, PORT))
+
+# Configure the server sockets
+# MAX_OPEN_REQUESTS connect requests before refusing outside connections
+serversocket.listen(MAX_OPEN_REQUESTS)
+
+print("Socket ready: {}".format(serversocket))
+
+while True:
+    # accept connections from outside
+    # The server is waiting for connections
+    print("Waiting for connections at {}, {} ".format(IP, PORT))
+    (clientsocket, address) = serversocket.accept()
+
+    # Connection received. A new socket is returned for communicating with the client
+    print("Attending connections from client: {}".format(address))
+
+    # Service the client
+    process_client(clientsocket)
