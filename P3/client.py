8c3de9c36fe43ea02c2e56d6945b227d71b49e92
ssxrgio <Pergamino27>
2019-03-06 17:00:23 +0100

    P3 update.

--- P3/client.py
+++ P3/client.py
@@ -1,15 +1,22 @@
 import socket
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print('Socket created succesfully.\n')
+
 port = 8081
-IP = '212.128.253.106'
+IP = '212.128.253.105'
 
 cond = True
 
 while cond:
-    sequence = input('Type a valid DNA sequence: ').upper()
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('\n---------------------------------\n  Socket created successfully.\n---------------------------------\n')
     s.connect((IP, port))
+
+
+    sequence = input('Type a valid DNA sequence: ').upper()
 
     if sequence == 'EXIT':
         print('\n-----------------------')
@@ -23,5 +30,3 @@ while cond:
         print(msg)
         s.close()
 
-    if msg[0:3] == 'len':
-        sequence = input('Choose a valid option from above: ').upper()
