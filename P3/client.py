33f21bd7a90e4c06943b7df014ab109f082fcf4a
ssxrgio <scarreras10@gmail.com>
2019-03-09 23:34:21 +0100

    .

--- P3/client.py
+++ P3/client.py
@@ -5,7 +5,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created succesfully.')
 
 port = 8081
-IP = '192.168.0.162'
+IP = '192.168.1.49'
+
 
 cond = True
 
@@ -13,15 +14,7 @@ while cond:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((IP, port))
 
-    lines = s.recv(2048).decode("utf-8")
-    mesg = s.recv(2048).decode("utf-8")
-    lines2 = s.recv(2048).decode('utf-8')
-
-    print(lines)
-    print(mesg)
-    print(lines2)
-
-    sequence = input('\nType a valid DNA sequence: ').upper()
+    sequence = input('\nType a valid DNA sequence followed by an operation by a \',\': ').upper()
 
     if sequence == 'EXIT':
         print('\n-----------------------')
