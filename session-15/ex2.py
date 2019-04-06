5a60d7936f8cdc975837c384aec461723fb363ed
ssxrgio <scarreras10@gmail.com>
2019-04-06 16:45:42 +0200

    Whole Session 15 Folder.

--- session-15/ex2.py
+++ session-15/ex2.py
@@ -2,7 +2,8 @@ import http.server
 import socketserver
 import termcolor
 
-PORT = 8075
+# Define the Server's port
+PORT = 8026
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
@@ -14,25 +15,41 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         termcolor.cprint(self.requestline, 'blue')
         if self.path == "/":
-            with open("ex1.html", "r") as c:
+            with open("ex2.html", "r") as c:
                 contents = c.read()
         elif "msg" in self.path:
-            with open("redirect_1.html", "r") as f:
-                echo_mess = self.path[self.path.find("=")+1:]
-                contents = f.read().format(echo_mess)
-                f.close()
+            if "chk" in self.path:
+                mess = self.path
+                mess1 = mess.split("&")
+                capital = mess1[0].upper()
+                with open("redirect_2.html", "r") as f:
+                    echo_mess = capital[capital.find("=")+1:]
+                    contents = f.read().format(echo_mess)
+                    f.close()
+            else:
+                with open("redirect_2.html", "r") as f:
+                    echo_mess = self.path[self.path.find("=")+1:]
+                    contents = f.read().format(echo_mess)
+                    f.close()
         else:
             with open("error.html", "r") as c:
                 contents = c.read()
                 c.close()
 
+
+
+
+        # -- We want to generate a response message with the following command
         self.send_response(200)
 
+        # --- Now we will define the content type and the header
         self.send_header('Content-Type', 'text/html')
         self.send_header('Content-Length', len(str.encode(contents)))
 
+        # We now finish the header
         self.end_headers()
 
+        # We will now send the body of the response se message
         self.wfile.write(str.encode(contents))
 
 
